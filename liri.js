3b24e4458c647953f6cc0ccf0e167cc9058a3c5c
Alan Montoya <alanmontoya2010@att.net>
2019-05-05 22:08:02 -0500

    variables added to liri.js

--- liri.js
+++ liri.js
@@ -1,10 +1,27 @@
+//TO READ AND SET ANY ENVIRONMENT VARIABLES WITH dotenv
 require("dotenv").config();
 
+//USE AXIOS FOR REQUESTS 
+let axios= require("axios");
+
+//FILE SYSTEMS STANARD TO NODE 
+const fs = require("fs");
+//IMPORT keys.js AND STORE AS VARIABLE 
 var keys = require("./keys.js");
 
-// Spotify
+// Spotify API and WRAPPER 
 const Spotify = require("node-spotify-api");
 const spotify = new Spotify(keys.spotify);
 
-// OMDB
-const request = require("request");
+// REQUIRE MOMENT 
+const moment = require("moment");
+
+//OMDB API
+let omdb = keys.omdb;
+
+//BANDS IN TOWN API 
+let bandsintown= keys.bandsintown;
+
+//USER INPUT AND COMMAND 
+let userInput=process.argv[2];
+let userQuery=process.argv[3];
